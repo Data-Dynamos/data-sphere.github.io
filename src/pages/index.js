28b8ccfb49da7e6b9e6edf1d895dd5afcbe7e1a9
samvardhan <samvardhan777@gmail.com>
2024-03-01 15:06:18 +0530

    change root url

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import  { Redirect } from 'react-router-dom';
 
 export default function Home() {
-  return <Redirect to='/docs/tour_agenda' />;
+  return <Redirect to='/data-sphere.github.io/docs/tour_agenda' />;
 }
